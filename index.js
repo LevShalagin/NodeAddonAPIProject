a7b0c806b4148748db6390118b076fa69abb9b58
a_LeoNardo_w <a.leonardo.w@yandex.ru>
2024-06-11 16:16:38 +0300

    install gatsby

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 ﻿var addon = require('bindings')('myAddon.node');
 
+
 //////////////////////////////////////////////////
 console.log("------------Test------------");
 console.log("JS: " + addon.get_date("01/01/2025", "Happy new year!"));
@@ -7,12 +8,13 @@ console.log("------------Test------------");
 //////////////////////////////////////////////////
 
 function addDate() {
-    
+
     let date = prompt("Введите дату").toString();
     let event = prompt("Введите событие").toString();
     
     let table = document.getElementById("tbody");
-    let row = addon.get_date(date, event);
+    let row = "<tr><td>" + date + "</td><td>" + event + "</td></tr>";
+    // get_date(date, event);
     console.log(row);
     console.log(table);
 
