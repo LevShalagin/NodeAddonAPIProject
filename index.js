72f270c17865cbce366f3ae42ae13db7c00143c8
a_LeoNardo_w <a.leonardo.w@yandex.ru>
2024-06-19 14:54:26 +0300

    last commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-﻿var addon = require('bindings')('myAddon.node');
+﻿const addon = require('bindings')('myAddon.node');
 
-
-//////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
 console.log("------------Test------------");
 console.log("JS: " + addon.get_date("01/01/2025", "Happy new year!"));
 console.log("------------Test------------");
-//////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////////////////////
 function addDate() {
 
     let date = prompt("Введите дату").toString();
@@ -25,3 +25,24 @@ function delDate() {
     let ind = prompt("Какое событие удалить?");
     document.getElementById("tbody").deleteRow(ind - 1);
 }
+////////////////////////////////////////////////////////////////////
+
+
+
+
+
+const fs = require('fs');
+const http = require('http');
+
+
+const port = 1217;
+const server = http.createServer( function (req, res){
+    fs.readFile('main.html', (err, data) => {
+    if (err) throw err; 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data); 
+    res.end();
+    });
+});
+
+server.listen(port, function(){ console.log("Сервер запущен по адресу http://localhost:" + port)});
