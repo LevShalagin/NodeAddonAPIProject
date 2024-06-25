5f4e1e87c66ed512c5aa6df84a9cb14625e4fd82
a_LeoNardo_w <a.leonardo.w@yandex.ru>
2024-06-25 13:49:26 +0300

    resetAll

--- addon.cc
+++ addon.cc
@@ -1,33 +1,15 @@
 ﻿#include <napi.h>
 
-Napi::Value getDate(const Napi::CallbackInfo& info) {
-    Napi::Env env = info.Env();
-
-    if (info.Length() < 2) {
-        Napi::TypeError::New(env, "Wrong number of arguments")
-            .ThrowAsJavaScriptException();
-        return env.Null();
-    }
-
-    char *date = (char*)malloc(1500);
-    char *event = (char*)malloc(1500);
-    char *result = (char*)malloc(1500);
-
-    strcpy(date, info[0].As<Napi::String>().Utf8Value().c_str());
-    strcpy(event, info[1].As<Napi::String>().Utf8Value().c_str());
-
-    sprintf(result, "<tr><td>%s</td><td>%s</td></tr>", date, event);
-
-    printf("C: %s\n", result);
-
-    Napi::String str = Napi::String::New(env, result);
-
-    return str;
+Napi::String SayHi(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  return Napi::String::New(env, "hi");
 }
+
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports.Set(Napi::String::New(env, "get_date"), Napi::Function::New(env, getDate));
-  return exports;
+    exports.Set(Napi::String::New(env, "hi"), Napi::Function::New(env, SayHi));
+    return exports;
 }
+
 
 NODE_API_MODULE(addon, Init)
